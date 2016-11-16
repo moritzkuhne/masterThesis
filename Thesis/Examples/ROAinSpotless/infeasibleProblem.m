fee5e7bdb494aa317d71bd12faed08c58e88bcef
fischer.gundlach <M.J.C.Fischer-Gundlach@student.tudelft.nl>
2016-11-16 15:02:50 +0100

    cleans previous version and adds comments
    adds problem description at the beginning of the section with expected
    solution
    
    in section one it is oberserved that unbounded primal problems are
    handles somehow -> write dual problem
    
    in second section results expect the expectencies

--- Thesis/Examples/ROAinSpotless/infeasibleProblem.m
+++ Thesis/Examples/ROAinSpotless/infeasibleProblem.m
@@ -1,9 +1,56 @@
-%% solving PSatz for V = x^2 =< rho and dynamics dx = x^2 - x
+%% the problem reads as:
+%                   
+%   min tr(I-Q0)    (=1-v1+1-v2+....1-vn)
+%   s.t. Q0 \in DD 
+%
+%  the solution is expected to look like: diagonal elements are inf
+%  but the solution are finite and scale reciprocal with dim(Q0)
+%  same happens when obj = tr(-Q0), it should be unbounded/dual infeasible
+%
+
 clear all; clc;
 
 % initiate polynomials
 x = msspoly('x',1);     %creates msspoly object   
-V = x;
+
+    % DSOS program
+    % Initialize program
+    prog = spotsosprog;
+    prog = prog.withIndeterminate(x);
+    deg = 3;
+    z = monomials(x,0:deg);
+    
+    [prog,Q0] = prog.newDD(deg+1); %constraining the DSOS multipliers
+    
+    % sedumi options
+    options = spot_sdp_default_options();
+    % Solve program
+    %sol = prog.minimizeDSOS(trace(eye(deg+1)-Q0), @spot_sedumi, options);
+    % Optimal value
+    opt_dsos0 = double(sol.eval(Q0))
+    trace(eye(deg+1)-opt_dsos0)
+    [feasibility,violation] = isDSOS(opt_dsos0)
+
+%% the problem reads as: V = diagonal Sum-of-Squares polynomial
+%   min tr(Q0)
+%   s.t. V-s0 \in DSOS
+%       Q0 \in DD (s0 = z'Q0z)
+%  
+%   the expected solution is all elemets of Q0 are zero CHECK
+%
+%   alternative:
+%   min tr(-Q0)
+%   s.t. V-s0 \in DSOS
+%       Q0 \in DD (s0 = z'Q0z)
+%  
+%   the expected solution is that s0 = V CHECK
+%
+
+clear all; clc;
+
+% initiate polynomials
+x = msspoly('x',1);     %creates msspoly object   
+V = x^2;
 
     % DSOS program
     % Initialize program
@@ -15,20 +62,14 @@ V = x;
     [prog,Q0] = prog.newDD(deg+1); s0 = z.'*Q0*z;   %constraining the DSOS multipliers
     
     % DSOS constraint
-    prog = prog.withDSOS((s0));
+    prog = prog.withDSOS((V-s0));
 
     % sedumi options
     options = spot_sdp_default_options();
     % Solve program
-    sol = prog.minimizeDSOS(trace(-Q0), @spot_sedumi, options);  
-    %sol = prog.minimizeDSOS(trace(eye(deg+1)-Q0), @spot_sedumi, options);
+    sol = prog.minimize(trace(-Q0), @spot_sedumi, options);  
   
     % Optimal value
     opt_dsos0 = double(sol.eval(Q0))
-    
+    trace(opt_dsos0)
     [feasibility,violation] = isDSOS(opt_dsos0)
-
-    
-
-
-
