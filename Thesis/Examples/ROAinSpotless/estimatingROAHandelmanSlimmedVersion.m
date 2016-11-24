7d334da8e74e8c6fd881fbf234ae3e8d33dda16b
fischer.gundlach <M.J.C.Fischer-Gundlach@student.tudelft.nl>
2016-11-24 15:18:12 +0100

    implements try-catch
    this version adds a try-catch block and increases deg of mMonoid.

--- Thesis/Examples/ROAinSpotless/estimatingROAHandelmanSlimmedVersion.m
+++ Thesis/Examples/ROAinSpotless/estimatingROAHandelmanSlimmedVersion.m
@@ -1,4 +1,4 @@
-%% solving Handelman Rep. for V = x^2 =< rho and dynamics dx = x^2 - x
+%% solving Handelman Rep. for V = x over x>=0
 clear all; clc;
 
 % initiate polynomials
@@ -25,15 +25,13 @@ while deg<2
         % add non-negative multipliers
         [prog,newlambda]...
             = prog.newPos(length(mMonoid)-length(prog.coneVar));
-        lambda = [lambda; newlambda]
-        full(prog.A)
+        lambda = [lambda; newlambda];
+        %full(prog.A)        %just for DEBUGGING
 
         % constrain to HR
         prog = prog.withPolyEqs(V-lambda.'*mMonoid)
-        V-lambda.'*mMonoid
-        full(prog.A)
-        prog = prog.withPolyEqs(V-lambda.'*mMonoid)
-        full(prog.A)
+        %V-lambda.'*mMonoid  %just for DEBUGGING
+        %full(prog.A)        %just for DEBUGGING
         
         % options
         options = spot_sdp_default_options();
@@ -52,68 +50,9 @@ while deg<2
     end
     
 end
-
-%sol = prog.minimize((0), @spot_sedumi, options);    
-
+  
 % Optimal value
 opt_lambda = double(sol.eval(lambda))
-
 [feasibility,~] = isDSOS(diag(opt_lambda));
 
 
-% %% solving Handelman Rep. for V = x^2 =< rho and dynamics dx = x^2 - x
-% clear all; clc;
-% 
-% % initiate polynomials
-% x = msspoly('x',1);     %creates msspoly object   
-% V = x;
-% poly = [x];
-% deg = 0;
-% 
-% % Initialize spotsosprog program
-% prog = spotsosprog;
-% prog = prog.withIndeterminate(x);
-% lambda = [];
-% 
-% while deg<2
-%     
-%     try 
-% 
-%         mMonoid = multiplicativeMonoid(poly, deg)
-% 
-%         % add non-negative multipliers
-%         [prog,newlambda]...
-%             = prog.newPos(length(mMonoid)-length(prog.coneVar));
-%         lambda = [lambda; newlambda]
-%         full(prog.A)
-% 
-%         % constrain to HR
-%         prog = prog.withPolyEqs(V-lambda.'*mMonoid)
-%         V-lambda.'*mMonoid
-%         full(prog.A)
-%         prog = prog.withPolyEqs(V-lambda.'*mMonoid)
-%         full(prog.A)
-%         
-%         % options
-%         options = spot_sdp_default_options();
-%         % Solve program
-%         sol = prog.minimize(sum(lambda), @spot_gurobi, options)
-%         
-%         if ~sol.isPrimalFeasible()
-%             error('Cannot evaluate: primal infeasible.'); 
-%         end
-%         
-%     catch ERROR
-%         deg = deg+1
-%         %prog.A = [];
-%         %prog.b = [];
-%     end
-%     
-% end
-% 
-% %sol = prog.minimize((0), @spot_sedumi, options);    
-% 
-% % Optimal value
-% opt_lambda = double(sol.eval(lambda))
-% 
-% [feasibility,~] = isDSOS(diag(opt_lambda));
