9b4fec9b2c34108ac01a1862b103f2128726ae03
fischer.gundlach <M.J.C.Fischer-Gundlach@student.tudelft.nl>
2016-12-07 14:29:45 +0100

    adds newDDSet which allows to create sets of DD matrixes

--- spotless-spotless_isos/spotless-spotless_isos/spotopt/@spotprog/spotprog.m
+++ spotless-spotless_isos/spotless-spotless_isos/spotopt/@spotprog/spotprog.m
@@ -561,6 +561,21 @@ classdef spotprog
 %            end
         end
         
+        function [prog,Qset] = newDDSet(prog,deg,n)
+        %NEWDDSET Returns a deg-by-deg-by-n msspoly array 
+        %   Along the dimensions n are DD matrixes e.g.:
+        %   newDDSet(2,2,2) returns an array Q where
+        %   Q(:,:,1) and Q(:,:,2) are 2-by-2 DD matrixes
+
+            Qset = cell(n,1);
+
+            for i=1:n
+                [prog,Qset{i}] = prog.newDD(deg); 
+            end
+
+            end
+        
+        
         function [prog,A,pvar] = newSDD(prog,n)
             if ~spotprog.isScalarDimension(n)
                 error('dim must be a row of non-negative scalars.');
