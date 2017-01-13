417fc75cab0c1e1d5153e490f26225a5e5a74896
fischer.gundlach <M.J.C.Fischer-Gundlach@student.tudelft.nl>
2017-01-13 10:59:32 +0100

    removed additional inequalities
    inequalities describing the box interval have been removed, in generell
    it is not possible to find explit foms of these.
    
    However, it should be possible to fit a square into a oval every time
    
    added a comment on the bottom

--- Thesis/Examples/example1/certain/Handelman.m
+++ Thesis/Examples/example1/certain/Handelman.m
@@ -18,13 +18,14 @@ rho_failed = rho_extr;  %lowest rho value for which prog. failed
 solution = solROAprog(-dV,rho_extr,'symm');
 
 %set the degree of the multiplicative monoid
-deg = 7;
+deg = 2;
 
 while ~(rho_try-solution.rho <= 0.01 && rho_try ~= 0) && ...
         ~(solution.rho_extr-solution.rho <= 0.01) && ... 
         ~(rho_failed-solution.rho <= 0.01)
     
-    inequalities = [rho_try-V;rho_try-x;x+rho_try];
+    inequalities = [rho_try-V;];
+    %inequalities = [rho_try-V;rho_try-x;x+rho_try];
 
     [sol,lambda] = HandelmanProg(-dV,inequalities,deg);
     feasibility = sol.isPrimalFeasible();
@@ -57,3 +58,7 @@ disp(['the estrimated ROA corresponds to rho = ',...
 %plottingV(solution.rho);
 %plottingdV(solution.rho);
 
+%% comments
+% This method will always fail, since deg(V_dot) = 3 and deg(V)=2,
+% hence all powers of (rho-V)^k with k \in N^+ produces even deg poly
+% Therefore it is not possible to equate V_dot
