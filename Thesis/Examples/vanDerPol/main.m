31e9d816d824dc0ebb7122eaeace560cd3a24c9f
fischer.gundlach <M.J.C.Fischer-Gundlach@student.tudelft.nl>
2017-02-03 14:38:03 +0100

    use new encapsulation
    this version makes use of the new encapsulaton

--- Thesis/Examples/vanDerPol/main.m
+++ Thesis/Examples/vanDerPol/main.m
@@ -7,31 +7,49 @@
 %                                                                        %       
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%draw phase portrai
-fig = phasePortrai2D(@eom);
+clear all; clc;
+disp('intialization');
 
-%this block was used once to determine an initial condition on the limit
-%cycle and the time of period
-Y0 = [2; 0.2036];
-tspan = [0 40];
-[T, Y, TE, YE, IE] = periodicity(Y0,tspan);
+system = @eom;
 
-%%
+
+%% draw phase portrai 
+disp('draw phase portrai')
+
+fig = phasePortrai2D(system);
+%% this block was used once to determine an initial condition on the limit
+%  cycle and the time of period
+% disp('determine periodicity and solution')
+% Y0 = [2; 0.2036];
+% tspan = [0 40];
+% [T, Y, TE, YE, IE] = periodicity(Y0,tspan);
+
+%% draw a trajectorie for the given initial values and timespan
+disp('calculate and draw periodic solution')
+
+%these were obtained by the periodicity function above
+Y0 = [2.003602513881589; 0.152170514312629]; 
 tspan = [0, 13.326573700340958/2];
-%draw a trajectorie for the given initial value
-Y0 = [2.003602513881589; 0.152170514312629];
-%tspan = [0, 20];
+
 hold on
-[t,y] = drawTrajectory(@eom,Y0,tspan);
+[t,y] = drawTrajectory(system,Y0,tspan);
+save('trajectory','t','y');
 
+%% draw transversal plains 
+disp('draw transversal plains')
 
-%%
-%draw transversal plains 
-tau = [0:0.1:13.326573700340958/2];
-result = ones(length(tau),1);
+tau = [0:0.1:tspan(end)];
+x_interp = interp1(t,y,tau);
+
 for i=1:length(tau)
-    result(i) = transverseDynamics(@eom,t,y,tau(i));
+   drawTransversalPlain2D(system,tau(i),x_interp(i,:));
 end
-%%
-%draw transverse dynamics
-%[ttrans,ytrans] = drawTransverseDynamics(Y0,tspan);
+
+%% calculate transverse dynamics
+disp('calculate transverse dynamics')
+
+disp('under construction')
+%LOOP FOR SPECIFIC TAU
+%i=1;
+%[dx_transverse] = transverseDynamics(system,x_interp(i,:),tau(i),x_transverse);
+%double(subs(dx_transverse,x_transverse,0.1))
