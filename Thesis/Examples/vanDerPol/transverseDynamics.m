8e612ebdde8024180603b97f796f9b388b5d7e9a
fischer.gundlach <M.J.C.Fischer-Gundlach@student.tudelft.nl>
2017-02-03 14:38:46 +0100

    new encapsulation
    this version makes use of the new capsulation structure.
    
    this function does acces a function to generate the transformation
    matrix

--- Thesis/Examples/vanDerPol/transverseDynamics.m
+++ Thesis/Examples/vanDerPol/transverseDynamics.m
@@ -1,43 +1,45 @@
-function [output] = transverseDynamics(system,t,y,tau)
-%drawTransverseDynamics for a transversal pertubation the dynamics are
-%calculated and drawn into the state space
-%   Detailed explanation goes here
+function [dx_transverse] = transverseDynamics(system,x,tau,x_transverse)
+%transversalDynamics for a transversal pertubation the dynamics are
+%calculated
 %   the notation is the same as in the literature study:
 %   z is a vector orthogonal to the transverse surface
-%   eta_n are vectors of the global coordinate system, eta_1 = [1 0 .. 0].' 
-%   zeta_n are the vectors of the transverse system, whereas zeta_1 is
-%   parallel to z and the other zetas span the transverse plane
-%   R is the roation matrix from global to transverse incl. normal vector z
 %   PI is the projection matrix from global into transverse
 
-tau = 0;
-load('trajectory.mat');
-system = @eom;
+[z,PI] = transversalPlains2D(system,tau,x);
 
-y_interp = interp1(t,y,tau);
+% we fixed z=f(x), therefore PI is independet of tau. 
+dtau_nom = z.'*system(tau,(x+transp(PI)*x_transverse));
+dtau_den = z.'*system(tau,x);
+dtau = dtau_nom/dtau_den;
 
-z = system(tau,y_interp)/norm(system(tau,y_interp)); %the surface is orthogonal to the trajectory
-eta_vectors = eye(length(z)); %vectors of global coordinate system, eta_i = eta_vectors(:,i) 
+dx_transverse_sum1 = PI*system(tau,(x+transp(PI)*x_transverse));
+dx_transverse_sum2 = -PI*system(tau,x)*dtau;
+dx_transverse = dx_transverse_sum1 + dx_transverse_sum2;
 
-%setting up the rotation matrix PI
-eta_1 = eta_vectors(:,1);
-R = zeros(length(eta_1));
 
-for i=1:length(eta_1)
-    for j=1:length(eta_1)
-        R(i,j) = eta_1(j)*z(i);
-    end
-end
 
-% constructing projection matrix
-zeta_vectors = zeros(length(z)); %vectors of the transv. coord. system zeta_i = zeta_vectors(:,i)
-for i=1:length(eta_vectors)
-    zeta_vectors(:,i) = R*eta_vectors(:,i);
-end
 
-PI = zeta_vectors(:,2:end).'
 
-output = PI*z;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
 
