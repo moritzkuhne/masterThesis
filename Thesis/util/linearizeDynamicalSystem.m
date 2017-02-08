a3360cd53aca3dcf436a9dbcec925dbb68a72651
fischer.gundlach <M.J.C.Fischer-Gundlach@student.tudelft.nl>
2017-02-08 11:25:05 +0100

    under construction
    this file is still under construction

--- Thesis/util/linearizeDynamicalSystem.m
+++ Thesis/util/linearizeDynamicalSystem.m
@@ -1,15 +1,20 @@
 %to do:     multivariable/multidimensional system
 %           whats about B C D
 
-function [dynamicalSystem_lin,A] = linearizeDynamicalSystem(dynamicalSystem)
+function [dynamicalSystem_lin,A] = linearizeDynamicalSystem(system,states)
 %LINEARIZEDYNAMICALSYSTEM This function returns a linearized system to an
 %mss-poly system
 %   So far, only passive systems are considered, e.g. it is only linearized
 %   for A matrix.
 %   More explenation here
 
-error('this function does not work for multidimensional systems')
+error('under construction')
 
+system = @eom;
+states = msspoly('x',2);
+
+dynamics = eom([],states)
+%%
 [indet,powers,M] = decomp(dynamicalSystem);
 
 for i=1:length(powers)
@@ -25,4 +30,16 @@ dynamicalSystem_lin = recomp(indet,powers,M);
 [~,~,A] = decomp(dynamicalSystem_lin);
 
 end
-
+% 
+% function dY = eom(T,Y,Y0)
+% Y1 = Y(1);
+% Y2 = Y(2);
+% 
+% %% van der pol osscillator
+% mu = 1;
+% 
+% Y1dot = Y2;
+% Y2dot = -Y1+mu*(1-Y1^2)*Y2;
+% 
+% dY = [Y1dot; Y2dot];
+% end
