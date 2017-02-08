0e1519f3ef76f0653db276c60e1a428395c30739
fischer.gundlach <M.J.C.Fischer-Gundlach@student.tudelft.nl>
2017-02-08 11:28:11 +0100

    length of drawn plains as parameter
    the length of the transverse plains which indicate region of stability
    is now passed to the draw function

--- Thesis/Examples/vanDerPol/drawTransversalPlain2D.m
+++ Thesis/Examples/vanDerPol/drawTransversalPlain2D.m
@@ -1,12 +1,12 @@
-function drawTransversalPlain2D(system,tau,x)
+function drawTransversalPlain2D(system,tau,x,lengthOfPlain)
 %DRAWTRANSVERSELPLAIN draws a transversal plain to the point x,tau
 
 [~,PI] = transversalPlains2D(system,tau,x);
 plain = PI.';
 
 %creating two points of transversal plain and draw line between them
-drawP1 = x.'-0.5*plain;
-drawP2 = x.'+0.5*plain;
+drawP1 = x.'-lengthOfPlain*plain;
+drawP2 = x.'+lengthOfPlain*plain;
 hold on
 plot([drawP1(1) drawP2(1)],[drawP1(2) drawP2(2)],'k');
 
