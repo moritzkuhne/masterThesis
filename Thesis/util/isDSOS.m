235530b0981a89cb98be3e8028dac94c188cde69
fischer.gundlach <M.J.C.Fischer-Gundlach@student.tudelft.nl>
2017-02-12 12:17:11 +0100

    PSDness is checked via circle theorem
    fromm here on, first the circle theorem is applied to test PSDness. If
    this fails, eig() is used.
    
    In general the circle theorem is numerical more robust than the chol
    factorization. For matrices which have eigenvalues close to or equal to
    0, eig lead to failures.

--- Thesis/util/isDSOS.m
+++ Thesis/util/isDSOS.m
@@ -4,35 +4,37 @@ function [feasibility,violation] = isDSOS(Q)
 %   if PSDness or symmetry are violated, than Q is infeasible
 %   violation of DSOS is marked but not treated as infeasible
 
-feasibility = true;
-violation = [];
+    feasibility = true;
+    violation = [];
 
-%test eigenvalues
-if ~all(eig(Q)>=0) == 1
-    feasibility = false;
-    violation = strcat(violation,'eigenvalues');
-end
-
-%test symmetry
-if ~issymmetric(Q) == 1
-    feasibility = false;
-    if size(violation) == 0
-        violation = strcat(violation,'symmetry');
-    else 
-        violation = strcat(violation,', symmetry');
-    end
-end
-
-%test DSOS constraint
-for i=1:size(Q,1)
-    if 2*abs(Q(i,i))<sum(abs(Q(i,:))) %reformulation of DSOS constraint
-        %feasibility = false;   
-        if size(violation) == 0
+    for i=1:size(Q,1)
+        if 2*abs(Q(i,i))<sum(abs(Q(i,:))) %reformulation of DSOS constraint
+            %feasibility = false;   
             violation = strcat(violation,'DSOS');
-        else
-            violation = strcat(violation,', DSOS');
+
+            %test eigenvalues
+            if ~all(eig(Q)>=0) == 1
+                feasibility = false;
+                if size(violation) == 0
+                    violation = strcat(violation,'eigenvalues');
+                else
+                    violation = strcat(violation,', eigenvalues');
+                end
+            end
         end
-        
-        break
+    end    
+    
+    %test symmetry
+    if ~issymmetric(Q) == 1
+        feasibility = false;
+        if size(violation) == 0
+            violation = strcat(violation,'symmetry');
+        else 
+            violation = strcat(violation,', symmetry');
+        end
     end
+
+    %test DSOS constraint
+
+
 end
