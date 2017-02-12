4a5bd4f30fe73d5347242c1df28ee9083bc1956a
fischer.gundlach <M.J.C.Fischer-Gundlach@student.tudelft.nl>
2017-02-12 12:11:56 +0100

    solver as option
    from here in the solver can be set via the option field option.solver.
    furthermore, the setting of solver and its options is incapsulated in
    solverOptionsPSDprog

--- Thesis/util/SprocedureProg.m
+++ Thesis/util/SprocedureProg.m
@@ -16,44 +16,39 @@ function [solution,Qset] = SprocedureProg(poly,inequalities,deg,options)
     end
     [indet,~,~] = decomp([poly; inequalities.']);
     z = monomials(indet,0:deg);
-    
+
     %initiate program
     prog = spotsosprog;
     prog = prog.withIndeterminate(indet);
-    
+
     % setting up DSOS polynomial multipliers
     [prog,Qset] = prog.newDDSet(length(z),length(inequalities));
-    
+
     for i=1:length(inequalities)
-        
+
         if ~exist('S', 'var')
             S = (z.'*Qset{i}*z)*inequalities(i);
         else
             S = S + (z.'*Qset{i}*z)*inequalities(i);
         end
-        
+
     end
-    
+
     % DSOS constraint
     prog = prog.withDSOS((poly-S));
-    
-    %set solver options
-    spotOptions = spot_sdp_default_options();
-    if isfield(options,'solverOptions')
-        spotOptions.solver_options = options.solverOptions;
-    else
-        spotOptions.solver_options = struct();
-    end
-    
+
+    %set solver and its options
+    [solver,spotOptions] = solverOptionsPSDProg(options);
+
     %define objective function
     if isfield(options,'objective')
         objective = objectiveROAProgDSOS(options.objective,Qset);
     else
         objective = 0;
     end
-    
+
     % Solve program
-    solution = prog.minimize(objective, @spot_gurobi, spotOptions); 
+    solution = prog.minimize(objective, solver, spotOptions);
     
 end
 
