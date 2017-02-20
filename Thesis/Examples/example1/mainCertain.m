1011ec35b143298cead3aeaad398ef05316e2e86
fischer.gundlach <M.J.C.Fischer-Gundlach@student.tudelft.nl>
2017-02-20 09:38:37 +0100

    change to dynamicalSystem object

--- Thesis/Examples/example1/mainCertain.m
+++ Thesis/Examples/example1/mainCertain.m
@@ -4,18 +4,21 @@ close all; clear all; clc;
 disp('Setting up the dynamical system.')
 % initiate polynomials
 x = msspoly('x',1);     %state variables 
+parameters = msspoly.empty;
 
 dx = x^2-x;
-V = x^2;
-dV = diff(V,x)*dx;
-
 inequalities = [];
+equalities = [];
+
+system = dynamicalSystem(dx,inequalities,equalities,x,parameters);
+
 
 %% setting options
 disp('Setting up the options for ROAprog.')
 
+options.rho = [0 1.1];
 options.lineSearchMethod = 'bisect';
-options.lineSearchMethodOptions = 'bisect'; %this line does not do anything yet
+options.lineSearchMethodOptions = 'random'; 
 options.objective = 'Lyap'; 
 options.feasibilityTest = 'analytical';
 
@@ -25,13 +28,15 @@ options.feasibilityTest = 'analytical';
 %setting solver options
 options.solver = @spot_gurobi;
 params.method = 0; %default 0 (primal simplex)
-params.FeasibilityTol = 1E-2; %default 1E-6
+params.FeasibilityTol = 1E-6; %default 1E-6
 params.outputFlag = 0;
 options.solverOptions = params;
 
-%method = @SprocedureProg; deg = 4; 
-%method = @kSprocedureProg; deg = 4; options.methodOptions.k = 2;
-%method = @PsatzProg; deg = 4; 
+options.method = @SprocedureProg; options.methodOptions.deg = 4; 
+%options.method = @kSprocedureProg; options.methodOptions.deg = 4; options.methodOptions.k = 2;
+%options.method = @PsatzProg; options.methodOptions.deg = 4; 
 
-[solution,options] = ROAProg(dV,V,inequalities,method,deg,options);
-disp('rho is'); solution.rho
+%% run ROAprog
+disp('Running the ROAprog.')
+[solution,options] = ROAProg(system,options);
+disp('The estimated region of attraction corresponds to rho = '); solution.rho
--- Thesis/Examples/example1/mainUncertain.m
+++ Thesis/Examples/example1/mainUncertain.m
@@ -7,17 +7,20 @@ x = msspoly('x',1);     %state variables
 a = msspoly('a',1);     %parameters
 
 dx = x^2-a*x;
-V = x^2;
-dV = diff(V,x)*dx;
-
 a_u = 1; a_l = 0.5;     %parameter bounds
 inequalities = [a-a_l, a_u-a];
+equalities = [];
+
+V = x^2;
+
+system = dynamicalSystem(dx,inequalities,equalities,x,a,V);
 
 %% setting options
-disp('Setting up the options for ROAprog.')
+disp('Setting the options for ROAprog.')
 
+options.rho = [0 0.5];
 options.lineSearchMethod = 'bisect';
-options.lineSearchMethodOptions = 'bisect'; %this line does not do anything yet
+options.lineSearchMethodOptions = 'random'; %this line does not do anything yet
 options.objective = 'Lyap'; 
 options.feasibilityTest = 'analytical';
 
@@ -27,15 +30,15 @@ options.feasibilityTest = 'analytical';
 %setting solver options
 options.solver = @spot_gurobi;
 params.method = 0; %default 0 (primal simplex)
-params.FeasibilityTol = 1E-2; %default 1E-6
+params.FeasibilityTol = 1E-6; %default 1E-6
 params.outputFlag = 0;
 options.solverOptions = params;
 
-method = @SprocedureProg; deg = 4; 
-%method = @kSprocedureProg; deg = 4; options.methodOptions.k = 2;
-%method = @PsatzProg; deg = 4; 
+%options.method = @SprocedureProg; options.methodOptions.deg = 4; 
+%options.method = @kSprocedureProg; options.methodOptions.deg = 4; options.methodOptions.k = 2;
+options.method = @PsatzProg; options.methodOptions.deg = 4; 
 
 %% run ROAprog
 disp('Running the ROAprog.')
-[solution,options] = ROAProg(dV,V,inequalities,method,deg,options);
+[solution,options] = ROAProg(system,options);
 disp('The estimated region of attraction corresponds to rho = '); solution.rho
