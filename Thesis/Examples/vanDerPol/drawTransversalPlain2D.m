4f74e0cdd891d86000e886ed6f66bcf476922a41
fischer.gundlach <M.J.C.Fischer-Gundlach@student.tudelft.nl>
2017-03-01 23:10:32 +0100

    adds line search
    first a bisection search is done, then fine line search

--- Thesis/Examples/vanDerPol/drawTransversalPlain2D.m
+++ Thesis/Examples/vanDerPol/drawTransversalPlain2D.m
@@ -1,5 +1,9 @@
-function drawTransversalPlain2D(system,tau,x,lengthOfPlain)
+function drawTransversalPlain2D(system,tau,x,lengthOfPlain,color)
 %DRAWTRANSVERSELPLAIN draws a transversal plain to the point x,tau
+
+if nargin < 5
+    color = 'k';
+end
 
 [~,PI] = transversalPlains2D(system,tau,x);
 plain = PI.';
@@ -8,6 +12,7 @@ plain = PI.';
 drawP1 = x.'-lengthOfPlain*plain;
 drawP2 = x.'+lengthOfPlain*plain;
 hold on
-plot([drawP1(1) drawP2(1)],[drawP1(2) drawP2(2)],'k');
+plot([drawP1(1) drawP2(1)],[drawP1(2) drawP2(2)],...
+    color,'linewidth',1.5,'MarkerSize',10);
 
 end
--- Thesis/Examples/vanDerPol/main.m
+++ Thesis/Examples/vanDerPol/main.m
@@ -36,13 +36,34 @@ hold on
 [t,y] = drawTrajectory(EOM,Y0,tspan);
 save('trajectory','t','y');
 
+%% setting options
+disp('Setting the options for ROAprog.')
+
+options.feasibilityTest = 'analytical';
+% options.feasibilityTest = 'numerical';
+
+%setting solver options
+options.solver = @spot_gurobi;
+params.FeasibilityTol = 1E-6; %default 1E-6
+params.outputFlag = 0;
+options.solverOptions = params;
+
+options.method = @SprocedureProg; options.methodOptions.deg = 4; %works fine
+params.method = 0; %default 0 (primal simplex)
+options.objective = 'Lyap'; 
+
+% options.method = @PsatzProg; options.methodOptions.deg = 4; 
+% params.method = 1;
+% options.objective = '0'; 
+
 %% calculate transverse dynamics
 disp('calculate transverse dynamics')
 
 tau = [0:0.1:tspan(end)];
 %tau = [0:0.07:tspan(end)]; %this one was good!
 x_interp = interp1(t,y,tau);
-rho_table = zeros(length(tau),1);
+rho_table_bisect = zeros(length(tau),1);
+rho_table_step = zeros(length(tau),1);
 
 for i=1:length(tau)
 %i=1; %LOOP FOR TAU 
@@ -68,39 +89,33 @@ system = dynamicalSystem(dx_transverse,inequalties,equalities,...
 %% find maximum level set of V for which system is stable
 disp('find maximum level set of V for which system is stable')
 
-%% setting options
-disp('Setting the options for ROAprog.')
+%% run ROAprog 
 
+%bisction search
 options.rho = [0 5];
 options.lineSearchMethod = 'bisect';
-options.lineSearchMethodOptions = 'random'; %this line does not do anything yet
-options.objective = 'Lyap'; 
-options.feasibilityTest = 'analytical';
+options.lineSearchMethodOptions = 'random'; 
 
-% options.objective = '0'; 
-% options.feasibilityTest = 'numerical';
-
-%setting solver options
-options.solver = @spot_gurobi;
-params.method = 0; %default 0 (primal simplex)
-params.FeasibilityTol = 1E-6; %default 1E-6
-params.outputFlag = 0;
-options.solverOptions = params;
-
-%options.method = @SprocedureProg; options.methodOptions.deg = 4; 
-%options.method = @kSprocedureProg; options.methodOptions.deg = 4; options.methodOptions.k = 2;
-options.method = @PsatzProg; options.methodOptions.deg = 4; 
-
-%% run ROAprog
 disp('Running the ROAprog.')
 [solution,options] = ROAProg(system,options);
-rho_table(i) = solution.rho;
+rho_table_bisect(i) = solution.rho;
+
+% %line search
+% options.rho = [rho_table_bisect(i) (norm(x_interp(i,:)))^2+0.1];
+% options.lineSearchMethod = 'step';
+% options.lineSearchMethodOptions = 'determined'; 
+% 
+% [solution,options] = ROAProg(system,options);
+% rho_table_step(i) = solution.rho;
 
 %% draw transversal plains 
 disp('draw transversal plains')
 
-length = sqrt(rho_table(i));
-drawTransversalPlain2D(EOM,tau(i),x_interp(i,:),length);
+length = sqrt(rho_table_bisect(i));
+drawTransversalPlain2D(EOM,tau(i),x_interp(i,:),length,'kx');
+
+% length = sqrt(rho_table_step(i));
+% drawTransversalPlain2D(EOM,tau(i),x_interp(i,:),length,'k');
 
 end
 
